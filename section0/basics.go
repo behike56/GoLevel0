c9439ecdadc0ef0d6507b94c3f2d3ebc96358db1
behike56 <living.tech.dancing.code@gmail.com>
2021-09-19 22:07:04 +0900

    init

---
+++ section0/basics.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+// 変数
+var n int = 100
+
+var m int
+m = 200
+
+// 型推論
+var l = 300
+
+// var 省略
+num := 350
+
+// まとめる
+var (
+	a = 100
+	b= 200
+)
+
+// 変数の０値
