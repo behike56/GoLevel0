c2614c0d82e2202da8aff8753d8de81beb28dc36
behike56 <living.tech.dancing.code@gmail.com>
2021-10-13 22:12:46 +0900

    add: Basics

---
+++ Basics/ArrayAndSlice.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main () {
+	/* 配列　*/
+	// 宣言と初期化
+	var arraysA [5]string
+	arraysA[0] = "Apple"
+	arraysA[1] = "Banana"
+	arraysA[2] = "Cactus"
+	arraysA[3] = "Deets"
+	arraysA[4] = "Eagle"
+
+	fmt.Println("arraysA: ", arraysA)
+	fmt.Println("length: ", len(arraysA), "capacity: ", cap(arraysA))
+
+	var arraysB [3]string = [3]string {"Apple", "Banana", "Cactus"}
+	fmt.Println("arraysB: ", arraysB)
+
+	arraysC := [...] string{"Apple", "Banana", "Cactus"}
+	fmt.Println("arraysC: ", arraysC)
+
+	arraysD := [3]string{}
+	fmt.Println("arraysD: ", arraysD)
+
+	arraysE := [...]int{3:15, 7:21}
+	fmt.Println("arraysE", arraysE)
+
+	// 配列の繰り返し処理
+	for idc, val := range arraysA {
+		fmt.Println("index is ", idc)
+		fmt.Println("value is ", val)
+	}
+
+	/* スライス  */
+	arraysF := []string{}
+	arraysF = append(arraysF, "Red")
+	arraysF = append(arraysF, "Green")
+	arraysF = append(arraysF, "Blue")
+}
