a61bb7512be61b553910546aea0c2b9c2cc4b632
behike56 <living.tech.dancing.code@gmail.com>
2021-10-16 23:38:11 +0900

    fix

--- Basics/ArrayAndSlice.go
+++ Basics/ArrayAndSlice.go
@@ -46,6 +46,25 @@ func main () {
 	fmt.Println("graph[][] ", graph)
 
 	/* スライス  */
+	var slice1 []int
+
+	// 配列の型、長さ、容量
+	slice2 = make([]int, 5, 10)
+
+	var slice3 = []int{1, 2, 3, 4, 5}
+
+	// 5番目が９９、７番目が１３０、その他が０の要素数８個のスライス
+	slice4 := []int{5: 99, 7: 130}
+
+	println(slice2[3])
+
+	// 長さ
+	println(len(slice2))
+	// 容量
+	println(cap(slice2))
+
+	slice3 = append(slice3, 6, 7)
+
 	arraysF := []string{}
 	arraysF = append(arraysF, "Red")
 	arraysF = append(arraysF, "Green")
